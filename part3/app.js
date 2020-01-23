618938d54c068e8f5c8f1ecd70bd0f2a24df8b8d
Mark Wild <mark.wild189@gmail.com>
2020-01-23 10:20:35 -0800

    commit

---
+++ part3/app.js
@@ -0,0 +1,129 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const cors = require('cors')
+const Contact = require('./models/contact')
+const mongoose = require('mongoose')
+
+app.use(bodyParser.json())
+morgan.token('data',(request)=>{
+    if(request.method=='POST')
+    return "::"+JSON.stringify(request.body.numbers)
+    else
+    return "::"
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
+app.use(cors())
+app.use(express.static('build'))
+
+app.get('/api/persons',(request,response)=>{
+    Contact.find({}).then(result=>{
+        response.json(result.map(contact => contact.toJSON()))
+    })
+})
+
+app.get('/info',(request,response)=>{
+    var d = Date(Date.now()); 
+    d = d.toString();
+    Contact.find({}).then(result=>{
+        response.send(
+            `<p>Phonebook has info for ${result.length} people</p>
+            ${d}
+            `
+        )
+    })
+})
+
+app.get('/api/persons/:id',(request,response)=>{
+    const id = mongoose.Types.ObjectId(request.params.id) 
+    console.log(id)
+    Contact.find({_id:id}).then(result=>{
+        if(result)
+        response.json(result[0])
+        else
+        response.status(404).end()
+    })
+})
+
+app.delete('/api/persons/:id',(request,response)=>{
+    Contact.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.post('/api/persons',(request,response,next)=>{
+    console.log("Posssttteeedd")
+    body = request.body;
+    if (!body.numbers) {
+        return response.status(400).json({ 
+          error: 'content missing' 
+        })
+    }
+    let number = body.numbers;
+    if (!number.name) {
+        return response.status(400).json({ 
+          error: 'name missing' 
+        })
+    }
+    else if (!number.number) {
+        return response.status(400).json({ 
+          error: 'number missing' 
+        })
+    }
+    Contact.find({number:number.number}).then(result=>{
+        if(result.length!==0)
+        {
+            return response.status(400).json({ 
+                error: 'number must be unique' 
+            })
+        }
+    })
+    const contact = new Contact({
+        name:number.name,
+        number:number.number
+    })
+    contact.save()
+    .then(result=>result.toJSON())
+    .then(result=>{response.json(result)})
+    .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+  
+    const contact = {
+      name: body.name,
+      number: body.number,
+    }
+  
+    Contact.findByIdAndUpdate(request.params.id, contact, { new: true })
+      .then(updatedContact => {
+        response.json(updatedContact.toJSON())
+      })
+      .catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError' && error.kind === 'ObjectId') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+    else if (error.name === 'ValidationError') {
+        console.log("validation error response");
+        return response.status(400).json({ error: error.message })
+    }
+  
+    next(error)
+}
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT,()=>{
+    console.log(`Listening to port ${PORT}`)
+})
---
+++ part3/build/precache-manifest.f28558e2201106e208460c5bc243f14a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d4040c3eeefb42c130d918510d83506",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46b1277bb14d844f8990",
+    "url": "/static/css/main.0a5c4e71.chunk.css"
+  },
+  {
+    "revision": "997bafc2fd5fb0a48772",
+    "url": "/static/js/2.18074af6.chunk.js"
+  },
+  {
+    "revision": "46b1277bb14d844f8990",
+    "url": "/static/js/main.d3d66171.chunk.js"
+  },
+  {
+    "revision": "643b1496a8e20922303c",
+    "url": "/static/js/runtime~main.bf59cc56.js"
+  }
+]);
